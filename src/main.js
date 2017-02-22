6a0e535514457f7c92e8b5d9f4ec2004e685bec1
Arthur Stolyar <nekr.fabula@gmail.com>
2017-02-22 19:39:48 +0300

    Add offline-online animation and github links

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ OfflinePlugin.install({
 var hexagon = document.querySelector('#hexagon');
 var offlineReady = document.querySelector('#offline-ready');
 var offlineReadyClose = document.querySelector('#offline-ready-close');
+var wifiShape = document.querySelector('#wifi-shape');
 
 (function() {
   var rotation = 0;
@@ -47,6 +48,34 @@ var offlineReadyClose = document.querySelector('#offline-ready-close');
 offlineReadyClose.addEventListener('click', function() {
   closeOfflineReady()
 });
+
+window.addEventListener('offline', function() {
+  goOffline();
+});
+
+window.addEventListener('online', function() {
+  requestAnimationFrame(function() {
+    wifiShape.classList.remove('wifi-offline');
+    requestAnimationFrame(function() {
+      wifiShape.classList.add('wifi-online');
+    });
+  });
+});
+
+if (!navigator.onLine) {
+  setTimeout(function() {
+    goOffline();
+  }, 300);
+}
+
+function goOffline() {
+  requestAnimationFrame(function() {
+    wifiShape.classList.remove('wifi-online');
+    requestAnimationFrame(function() {
+      wifiShape.classList.add('wifi-offline');
+    });
+  });
+}
 
 function openOfflineReady() {
   requestAnimationFrame(function() {
